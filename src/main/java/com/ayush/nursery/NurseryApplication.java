b55f153f54d20f7b5c99bfa70ed2dac1a270cfef
Mehul Jain <97732513+mehuljain55@users.noreply.github.com>
2025-12-12 20:47:19 +0530

    update

--- src/main/java/com/ayush/nursery/NurseryApplication.java
+++ src/main/java/com/ayush/nursery/NurseryApplication.java
@@ -2,8 +2,10 @@ package com.ayush.nursery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.ayush.nursery")
 public class NurseryApplication {
 
 	public static void main(String[] args) {
